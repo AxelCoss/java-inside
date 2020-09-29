38ceef9376350e313f933449010569a31097d2ef
AxelCos <axel.cossinet@gmail.com>
2020-09-29 10:46:23 +0200

    Add JUnit Test, Put Tests Record in the Test class

--- lab2/src/main/java/fr/umlv/javainside/JSONPrinter.java
+++ lab2/src/main/java/fr/umlv/javainside/JSONPrinter.java
@@ -1,22 +1,45 @@
 package fr.umlv.javainside;
 
+import java.io.UncheckedIOException;
 import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.lang.reflect.RecordComponent;
+import java.lang.reflect.UndeclaredThrowableException;
 import java.util.Arrays;
 import java.util.stream.Collectors;
 
 public class JSONPrinter {
     public static String toJSON(Record record) {
-        return Arrays.stream(record.getClass().getRecordComponents())
-                .map(recordComponent -> {
-                    try {
-                        return recordComponent.getName() +
-                                ' ' +
-                                recordComponent.getAccessor().invoke(record);
-                    } catch (IllegalAccessException | InvocationTargetException e) {
-                        System.err.println(e.getMessage());
-                        return "";
-                    }
-                })
-                .collect(Collectors.joining(", "));
+        return "{"+ Arrays.stream(record.getClass().getRecordComponents())
+                .map(recordComponent -> '\"' +
+                        recordComponent.getName() +
+                        "\": " +
+                        addQuoteIfIsStringTypeComponent(recordComponent) +
+                        invokeAccessor(recordComponent.getAccessor(), record) +
+                        addQuoteIfIsStringTypeComponent(recordComponent)
+                ).collect(Collectors.joining(", "))
+                + " }";
+    }
+
+    private static char addQuoteIfIsStringTypeComponent(RecordComponent recordComponent) {
+        if (recordComponent.getType().equals(String.class)) {
+            return '\"';
+        }
+        return '\0';
+    }
+
+    private static Object invokeAccessor(Method accessor, Record record) {
+        try {
+            return accessor.invoke(record);
+        } catch (IllegalAccessException e) {
+            throw (IllegalAccessError) new IllegalAccessError().initCause(e);
+        } catch (InvocationTargetException e) {
+            Throwable cause = e.getCause();
+            if (cause instanceof RuntimeException runtimeException)
+                throw runtimeException;
+            if (cause instanceof Error error)
+                throw new RuntimeException(error);
+            throw new UndeclaredThrowableException(cause);
+        }
     }
 }
