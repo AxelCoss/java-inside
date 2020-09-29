ab7b6f61ec297d87757554f317121bea022341a1
AxelCos <axel.cossinet@gmail.com>
2020-09-29 09:38:19 +0200

    Add compoent value in the toJSON method

--- lab2/src/main/java/fr/umlv/javainside/JSONPrinter.java
+++ lab2/src/main/java/fr/umlv/javainside/JSONPrinter.java
@@ -1,13 +1,22 @@
 package fr.umlv.javainside;
 
-import java.lang.reflect.RecordComponent;
+import java.lang.reflect.InvocationTargetException;
 import java.util.Arrays;
 import java.util.stream.Collectors;
 
 public class JSONPrinter {
     public static String toJSON(Record record) {
         return Arrays.stream(record.getClass().getRecordComponents())
-                .map(RecordComponent::getName)
+                .map(recordComponent -> {
+                    try {
+                        return recordComponent.getName() +
+                                ' ' +
+                                recordComponent.getAccessor().invoke(record);
+                    } catch (IllegalAccessException | InvocationTargetException e) {
+                        System.err.println(e.getMessage());
+                        return "";
+                    }
+                })
                 .collect(Collectors.joining(", "));
     }
 }
